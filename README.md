04a62cfe1b63c78f8b70aa067dafea032cb51b7e
Denis S. Badanin <grafforever@gmail.com>
2022-06-30 21:53:33 +0300

    moar preparation automatization

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 Terraform &amp; Ansible infrastucture
 
 
-####before start
-put `erraform-provider-proxmox_v2.9.10` into `~/.terraform.d/plugins/my.pc.local/telmate/proxmox/2.9.10/linux_amd64`
+#### before start
+edit `prepare.sh` set token id in `TID` and token secred in `TPWD`
 
-####before apply
-
-```
-export PM_API_TOKEN_ID=<token>
-export PM_API_TOKEN_SECRET=<secret>
-```
 
--- prepare.sh
+++ prepare.sh
@@ -13,6 +13,22 @@ UPWD="secret"
 #------setup done-------------
 
 
+
+#check terraform plugin exists
+PATH_TO_CHECK=~/.terraform.d/plugins/my.pc.local/telmate/proxmox/2.9.10/linux_amd64/
+FILE_TO_CHECK="$PATH_TO_CHECK"/terraform-provider-proxmox_v2.9.10
+if [[ -f "$FILE_TO_CHECK" ]]; then
+  echo "proxmox file exists"
+else
+  echo "proxmox file not exists downloading"
+  mkdir -p "$PATH_TO_CHECK"
+  wget https://github.com/Telmate/terraform-provider-proxmox/releases/download/v2.9.10/terraform-provider-proxmox_2.9.10_linux_amd64.zip
+  unzip terraform-provider-proxmox_2.9.10_linux_amd64.zip -d "$PATH_TO_CHECK"
+  rm -rf terraform-provider-proxmox_2.9.10_linux_amd64.zip
+fi
+
+
+
 #most default shell rc
 SHELLRC='.bashrc'
 #swhitch to most beauty shell rc
