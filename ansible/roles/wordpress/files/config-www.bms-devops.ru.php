9fb836d92c78a822dad65acc146836014bc51859
Denis S. Badanin <otd105@npcap.ru>
2022-07-07 12:32:00 +0300

    wordpress works fine

--- ansible/roles/wordpress/files/config-www.bms-devops.ru.php
+++ ansible/roles/wordpress/files/config-www.bms-devops.ru.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress');
-define('DB_PASSWORD', 'wordress');
+define('DB_PASSWORD', 'wordpress');
 define('DB_HOST', '172.16.0.101');
 define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
 ?>
